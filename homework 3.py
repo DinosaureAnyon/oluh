cda8944f940ae4df3b6c1cdec113908615f024aa
Dinosaure Anyon <paskovvitala235@gmail.com>
2024-10-03 23:44:44 +0300

    Add files via upload

---
+++ homework 3.py
@@ -0,0 +1,20 @@
+# n = int(input('Введите число\n'))
+# z = 0
+# for i in range(1, n+1):
+#     z += i ** 3
+# print(f'Сумма кубов от 1 до {n} = {z}')
+#
+#
+#
+# q = ''
+# t = 1
+# for j in range(1, 10):
+#     for i in range(1,10):
+#         i = i*j
+#         if len(str(i)) == 1:
+#             q = q + str(i) + '  '
+#         else:
+#             q = q + str(i) + ' '
+#
+#     print(q)
+#     q = ''
